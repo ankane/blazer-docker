73423b467a06a1b016e3c6b2203d568f16a0bc37
Andrew Kane <andrew@chartkick.com>
2020-09-04 14:44:56 -0700

    Added health section to docs [skip ci]

--- README.md
+++ README.md
@@ -36,6 +36,10 @@ Add basic authentication with:
 
 Or use a reverse proxy like [OAuth2 Proxy](https://github.com/oauth2-proxy/oauth2-proxy).
 
+## Health
+
+Use the `/health` endpoint to check if the app is running and can connect to the database.
+
 ## Checks
 
 TODO: SMTP instructions
--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ abort "No DATABASE_URL" unless ENV["DATABASE_URL"]
 module BlazerSolo
   class Application < Rails::Application
     routes.append do
-      # checks app is up, not data sources healthy
+      # checks app is up and can connect to database
+      # does not check data sources
       # not protected by auth, so do not expose data
       get "health", to: ->(env) {
         if Blazer::Connection.connection.active?
