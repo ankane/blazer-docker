f2c2c81477458bab007778c43a8cebb58b3642ee
Andrew Kane <andrew@ankane.org>
2026-07-28 03:37:29 -0700

    Updated style [skip ci]

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,11 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+workers Integer(ENV["PUMA_WORKERS"] || 3)
+threads Integer(ENV["MIN_THREADS"]  || 1), Integer(ENV["MAX_THREADS"] || 16)
 
 preload_app!
 
-if ENV['BIND']
-  bind ENV['BIND']
+if ENV["BIND"]
+  bind ENV["BIND"]
 else
-  port ENV['PORT'] || 3000
+  port ENV["PORT"] || 3000
 end
-environment ENV['RACK_ENV'] || 'production'
+environment ENV["RACK_ENV"] || "production"
